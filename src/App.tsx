996706087217837f49695485b9e0639de9c1e4d6
DevMaan707 <dev.maan707@gmail.com>
2024-12-01 23:17:53 +0530

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 import CodeEditor from "./components/CodeEditor";
 function App() {
   return (
+
     <main>
+      <div>
+      <Button>Click me</Button>
+    </div>
       <h1 className=" bg-blue-600 text-3xl font-bold underline">
       Hello world!
       </h1>
